8520883cd6a9662781c4615d8c53a317696eced6
Jonathan Benjamin <ppcasm@gmail.com>
2014-10-06 19:01:47 -0400

    Update patchdrgn.sh

--- drgnpatch/patchdrgn.sh
+++ drgnpatch/patchdrgn.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 # Apply dynamic patches to support programming N64 with GameShark
 # This file is executed from inside the libdragon source tree directory
 
@@ -7,3 +7,4 @@
 # sit inside of the libdragon folder before it is built, and get/copy/etc
 # any file over the libdragon source tree in order to create a working
 # GameShark programming environment. -ppcasm
+echo "Applying GameShark development related patches to libdragon source tree"
